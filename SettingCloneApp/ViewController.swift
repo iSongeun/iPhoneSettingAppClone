d0022940c5c26a548bf814a1727b11d529e7f0cf
luckybsmith <vs1707@naver.com>
2023-03-13 18:03:09 +0900

    Initial Commit

---
+++ SettingCloneApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SettingCloneApp
+//
+//  Created by 이송은 on 2023/03/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
